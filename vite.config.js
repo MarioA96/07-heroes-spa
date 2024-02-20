0b3d2cf18462904b415142b47c140ffb2182f2be
MarioA96 <marioaquirozs@gmail.com>
2024-02-20 01:45:18 -0600

    Updated files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://marioa96.github.io/07-heroes-spa/',
 })
