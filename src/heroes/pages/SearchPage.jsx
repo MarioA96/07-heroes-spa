bfa27fbc18333360b561a291980b562ee92d3bfc
MarioA96 <marioaquirozs@gmail.com>
2024-03-01 16:22:05 -0600

    Modificación de archivos
    Se modifico la dependencia 'query-string' por un downgrade, debido a un breaking change.

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -39,7 +39,7 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form onSubmit={ onSearchSubmit }>
+                    <form aria-label="form" onSubmit={ onSearchSubmit }>
                         <input 
                             type="text"
                             placeholder="Search a hero"
@@ -74,7 +74,8 @@ export const SearchPage = () => {
                     >
                              Search a hero 
                     </div>
-                    <div 
+                    <div
+                        aria-label="search-error" 
                         className="alert alert-danger animate__animated animate__fadeIn" 
                         style={{ display: showError ? '' : 'none' }}
                     >
