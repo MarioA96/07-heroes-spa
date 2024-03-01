115b561bd6b3440a9186ebe49f5710b531849388
MarioA96 <marioaquirozs@gmail.com>
2024-03-01 16:25:18 -0600

    Adición de archivos
    Se agregaron archivos de configuración para la pruebas unitarias e integración de la aplicación.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFiles: ['./jest.setup.js'],
+    // transformIgnorePatterns: [
+    //     "node_modules/(?!(decode-uri-component|query-string)/)"
+    // ]
+    // transformIgnorePatterns: [
+    //     "node_modules/(?!(query-string|decode-uri-component)).+\\.js$"
+    // ]
+}
